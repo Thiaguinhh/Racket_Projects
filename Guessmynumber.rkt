d77d7db893b8804a5f6d5f7bf3ba09c2dec7ac9f
Thiaguinhh <105451512+Thiaguinhh@users.noreply.github.com>
2022-05-12 14:45:25 -0300

    Update Guessmynumber.rkt

--- Guessmynumber.rkt
+++ Guessmynumber.rkt
@@ -1,8 +1,6 @@
 #lang racket
 (define lower 1)
-lower
 (define upper 100)
-upper
 (define (guess)
   (quotient (+ lower upper) 2))
 (define (smaller)
@@ -10,4 +8,8 @@ upper
   (guess))
 (define (bigger)
   (set! lower (min upper (add1 (guess))))
+  (guess))
+(define (start n m)
+  (set! lower (min n m))
+  (set! upper (max n m))
   (guess))
